545c0ebaa5b4b8701ba1cb283e13c4e89628de91
miloshIra <maritonski@gmail.com>
2022-07-12 09:50:13 +0200

    Django devsearch project for work.

---
+++ devsearch/projects/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from .models import Project, Tag
+from .forms import ProjectForm
+from .utils import search_project, paginate_projects
+
+
+def projects(request):
+    projects, search_query = search_project(request)
+    custom_range, projects = paginate_projects(request, projects, 2)
+    context = {'projects': projects, 'search_query': search_query, 'custom_range': custom_range}
+    return render(request, 'projects/projects.html', context)
+
+
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    return render(request, 'projects/single-project.html', {'project': projectObj,})
+
+
+@login_required(login_url='login')
+def create_project(request):
+    profile = request.user.profile
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
+            return redirect('account')
+
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
+
+
+@login_required(login_url='login')
+def update_project(request, pk):
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('account')
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
+
+
+@login_required(login_url='login')
+def delete_project(request, pk):
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('account')
+    context = {'object': project}
+    return render(request, 'delete_template.html', context)
+
+
